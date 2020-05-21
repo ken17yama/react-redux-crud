6e8964f665644a507758cab494d1bae4ae1e7e58
ken17yama <ken17yama@gmail.com>
2020-05-21 13:38:03 +0900

    新規作成機能の実装

--- src/components/events-new.js
+++ src/components/events-new.js
@@ -1,17 +1,59 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
 
-// import { postEvent } from '../actions';
+import { postEvent } from '../actions';
 
-class Eventsnew extends Component {
-  render() {
+class EventsNew extends Component {
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  renderField(field) {
+    const { input, label, type, meta: { touched, error } } = field
+
     return (
-      <React.Fragment><div>foo</div>
+      <div>
+        <input {...input} placeholder={label} type={type} />
+        {touched && error && <span>{error}</span>}
+      </div>
+    )
+  }
+
+  async onSubmit(values) {
+    await this.props.postEvent(values)
+    this.props.history.push('/')
+  }
+
+  render() {
+    const { handleSubmit } = this.props
+
+    return (
+      <React.Fragment>
+        <form onSubmit={handleSubmit(this.onSubmit)}>
+          <Field label='Title' name="title" type="text" component={this.renderField} />
+          <Field label='Body' name="body" type="text" component={this.renderField} />
+          <input type="submit" value="submit" disabled={false} />
+          <Link to='/'>Cansel</Link>
+        </form>
       </React.Fragment >
     )
   }
 }
-// const mapDispatchToProps = ({ postEvent })
 
-export default connect(null, null)(Eventsnew)
+const validate = values => {
+  const errors = {}
+
+  if (!values.title) errors.title = "Titleを入力してください。"
+  if (!values.body) errors.body = "Bodyを入力してください。"
+
+  return errors;
+}
+
+const mapDispatchToProps = ({ postEvent })
+
+export default connect(null, mapDispatchToProps)(
+  reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+)
