52ea54502624741928ea66bad9681219f3bc9f9d
ken17yama <ken17yama@gmail.com>
2020-05-21 11:23:27 +0900

    新規作成画面の表示

---
+++ src/components/events-new.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom'
+
+// import { postEvent } from '../actions';
+
+class Eventsnew extends Component {
+  render() {
+    return (
+      <React.Fragment><div>foo</div>
+      </React.Fragment >
+    )
+  }
+}
+// const mapDispatchToProps = ({ postEvent })
+
+export default connect(null, null)(Eventsnew)
